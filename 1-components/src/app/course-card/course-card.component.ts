4193ff7778c902ef723129cae4f0c1faeaec7ae4
Charlie Osorio <caosmo@outlook.com>
2025-02-09 15:03:29 +0100

    Update course angular project

--- 1-components/src/app/course-card/course-card.component.ts
+++ 1-components/src/app/course-card/course-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
+import { NgClass, NgIf, NgStyle } from "@angular/common";
 
 @Component({
   selector: "course-card",
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
+  imports: [NgIf, NgClass, NgStyle],
 })
 export class CourseCardComponent {
   @Input({
@@ -21,5 +23,17 @@ export class CourseCardComponent {
   onCourseViewed() {
     console.log("Course card component - Button Clicked...");
     this.courseSelected.emit(this.course);
+  }
+
+  onSetCardClasses() {
+    if (this.course.category === "BEGINNER") {
+      return "beginner";
+    }
+  }
+
+  onSetCardStyle() {
+    return {
+      "text-decoration": "underline",
+    };
   }
 }
