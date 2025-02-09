1e1fdbb710471eea411eff9382fdb61fe3cc186f
Charlie Osorio <caosmo@outlook.com>
2025-02-09 01:23:02 +0100

    @For Directive new way

--- 1-components/src/app/app.component.ts
+++ 1-components/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
 export class AppComponent {
+  courses = COURSES;
 
-
-
+  onCourseSelected(course: Course) {
+    console.log("Course app component - Button Clicked...", course);
+  }
 }
---
+++ 1-components/src/app/course-card/course-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
+})
+export class CourseCardComponent {
+  @Input({
+    required: true,
+  })
+  course: Course;
+
+  @Input({ required: true })
+  index: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    console.log("Course card component - Button Clicked...");
+    this.courseSelected.emit(this.course);
+  }
+}
