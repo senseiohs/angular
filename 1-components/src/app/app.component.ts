27bbfe2e99b8236843d8f4d29cd9d6f559095cf6
Charlie Osorio <caosmo@outlook.com>
2025-02-12 21:50:48 +0100

    Implementation pipes templateç

--- 1-components/src/app/app.component.ts
+++ 1-components/src/app/app.component.ts
@@ -11,6 +11,11 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2024, 2, 12); //Date
+  totalCoffeExport = 0.76; //percent
+  salary = 100000; //currency
+  amountEletrocit = 3.755435334; //Decimal
+
   onCourseSelected(course: Course) {
     console.log("Course app component - Button Clicked...", course);
   }
