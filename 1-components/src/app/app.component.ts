b6acbb7657d885559afec3c571891791b21790d1
Charlie Osorio <caosmo@outlook.com>
2025-02-16 14:19:40 +0100

    Projection content

--- 1-components/src/app/app.component.ts
+++ 1-components/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ViewContainerRef } from "@angular/core";
 import { COURSES } from "../db-data";
 import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
   selector: "app-root",
@@ -11,12 +12,16 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
+  @ViewChild("firtCard")
+  firstCard: CourseCardComponent;
+
   startDate = new Date(2024, 2, 12); //Date
   totalCoffeExport = 0.76; //percent
   salary = 100000; //currency
   amountEletrocit = 3.755435334; //Decimal
 
   onCourseSelected(course: Course) {
+    console.log(this.firstCard.course);
     console.log("Course app component - Button Clicked...", course);
   }
 }
