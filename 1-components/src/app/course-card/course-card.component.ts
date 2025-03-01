31710dcc7e32d32d37a04414d430d480edf28488
Charlie Osorio <caosmo@outlook.com>
2025-03-01 20:14:49 +0100

    ng-template

--- 1-components/src/app/course-card/course-card.component.ts
+++ 1-components/src/app/course-card/course-card.component.ts
@@ -1,18 +1,18 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { Course } from "../model/course";
 import {
-  NgClass,
-  NgIf,
-  NgStyle,
-  NgSwitch,
-  NgSwitchCase,
-} from "@angular/common";
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+} from "@angular/core";
+import { Course } from "../model/course";
+import { NgClass, NgIf, NgStyle, NgTemplateOutlet } from "@angular/common";
 
 @Component({
   selector: "course-card",
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
-  imports: [NgIf, NgClass, NgStyle],
+  imports: [NgIf, NgClass, NgStyle, NgTemplateOutlet],
 })
 export class CourseCardComponent {
   @Input({
@@ -22,6 +22,9 @@ export class CourseCardComponent {
 
   @Input({ required: true })
   index: number;
+
+  @Input()
+  noImageTpl: TemplateRef<any>;
 
   @Output()
   courseSelected = new EventEmitter<Course>();
