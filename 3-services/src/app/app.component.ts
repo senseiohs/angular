b8432b5c2188f56e18264955c2ef53739d970b0b
Charlie Osorio <caosmo@outlook.com>
2025-03-30 14:04:28 +0200

    Simplified provider configuration services

--- 3-services/src/app/app.component.ts
+++ 3-services/src/app/app.component.ts
@@ -15,21 +15,6 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
 import { CoursesService } from "./services/courses.service";
-import { HttpClient } from "@angular/common/http";
-import { inject } from "@angular/core/testing";
-
-//Factoria para crear el servicio de cursos
-// Este servicio se inyecta en el componente AppComponent
-// y se utiliza para cargar los cursos desde el servidor
-// y para actualizar los cursos en el servidor
-function courseServiceProvider(http: HttpClient) {
-  return new CoursesService(http);
-}
-
-// Token para inyectar el servicio de cursos el cual debe ser único
-export const COURSES_SERVICE = new InjectionToken<CoursesService>(
-  "COURSES_SERVICE"
-);
 
 @Component({
   selector: "app-root",
@@ -38,22 +23,16 @@ export const COURSES_SERVICE = new InjectionToken<CoursesService>(
   standalone: false,
   providers: [
     {
-      provide: COURSES_SERVICE, // Token que identifica el provider del courseService
-      useFactory: courseServiceProvider, // Función que crea el courseService
-      deps: [HttpClient], // Dependencias del courseService que requiere en el constructor
+      provide: CoursesService, // Con la clase ya no necesitamos la funcion courseServiceProvider
+      useClass: CoursesService, // Con esto ya no necesitamos el token COURSES_SERVICE
     },
   ],
 })
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
 
-  //Inyectamos el servicio de cursos en el constructor utilizando el token COURSES_SERVICE
-  // Esto permite que el servicio sea único y no se comparta entre diferentes instancias
-  // de AppComponent, pero si lo necesitaramos en un ChildComponent solo lo inyectaríamos
-  // en el constructor del ChildComponent invocando al mismo token COURSES_SERVICE
-  constructor(
-    @Inject(COURSES_SERVICE) private readonly coursesService: CoursesService
-  ) {}
+  // Ya no necesitamos inyectar el token COURSES_SERVICE
+  constructor(private readonly coursesService: CoursesService) {}
 
   ngOnInit() {
     this.courses$ = this.coursesService.loadCourses();
