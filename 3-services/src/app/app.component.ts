44f71d6fa5a9d42e3df1be6411c9e3044a500dd9
Charlie Osorio <caosmo@outlook.com>
2025-04-05 17:54:30 +0200

    Custom Change Detection with CnageDetectorRef

--- 3-services/src/app/app.component.ts
+++ 3-services/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
+  DoCheck,
   Inject,
   OnInit,
 } from "@angular/core";
@@ -14,20 +16,35 @@ import { AppConfig, CONFIG_TOKEN } from "src/tools/configuration";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   standalone: false,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush, //Debemos declarar changeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
   courses: Course[];
+  loaded = false;
 
   constructor(
     private readonly coursesServiceFather: CoursesService,
-    @Inject(CONFIG_TOKEN) private readonly config: AppConfig
+    @Inject(CONFIG_TOKEN) private readonly config: AppConfig,
+    private cd: ChangeDetectorRef
   ) {}
 
+  ngDoCheck(): void {
+    console.log("ngDoCheck()");
+
+    //No funciona - que chafa
+    if (this.loaded) {
+      this.cd.markForCheck();
+      console.log("called cd.markForCheck()");
+      this.loaded = undefined;
+    }
+  }
+
   ngOnInit() {
-    this.coursesServiceFather
-      .loadCourses()
-      .subscribe((response) => (this.courses = response));
+    this.coursesServiceFather.loadCourses().subscribe((response) => {
+      this.courses = response;
+      this.cd.markForCheck();
+      // this.loaded = true;
+    });
   }
 
   OnEditTitleCourse() {}
--- 3-services/src/app/course-card/course-card.component.ts
+++ 3-services/src/app/course-card/course-card.component.ts
@@ -1,5 +1,7 @@
 import {
   Attribute,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
@@ -13,6 +15,7 @@ import { Course } from "../model/course";
   templateUrl: "./course-card.component.html",
   styleUrls: ["./course-card.component.css"],
   standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseCardComponent implements OnInit {
   @Input()
@@ -24,7 +27,7 @@ export class CourseCardComponent implements OnInit {
   @Output("courseChanged")
   courseEmitter = new EventEmitter<Course>();
 
-  constructor(@Attribute("type") type: string) {}
+  constructor(@Attribute("type") type: string, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {}
 
